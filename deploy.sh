62f979bcc8b51bdcfd4733708e8baebba8121c3a
Dmitri Sivachenko <d.sivachenko@hotmail.com>
2017-12-13 04:13:20 +0300

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ zip -r build.zip build
 curl  -H "Content-type: application/zip" \
       -H "Authorization: Bearer $NETLIFY_KEY" \
       --data-binary "@build.zip" \
-      http://api.netlify.com/api/v1/sites/loftschool-react-hw-lofttrade.netlify.com/deploys
+      http://api.netlify.com/api/v1/sites

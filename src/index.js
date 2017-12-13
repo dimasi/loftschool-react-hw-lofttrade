d98a86d440eb1b7e0a163ca3135fb6499d861605
Dmitri Sivachenko <d.sivachenko@hotmail.com>
2017-12-13 04:12:02 +0300

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import createStore from './store';
 import {Provider} from 'react-redux';
-import registerServiceWorker from './registerServiceWorker';
 import App from 'Components/App';
 import './index.css';
 
@@ -17,5 +16,3 @@ ReactDOM.render(
   </BrowserRouter>, 
   document.getElementById('root')
 );
-
-registerServiceWorker();
